cdf4c24bcd49fad13d64d0ce2fa25431b8e198ae
Kiryl Harbacheuski <kiryl.harbacheuski@soft-fx.lv>
2022-08-18 15:56:10 +0300

    project works

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
     entry: "./src/index.ts",
     output: {
-      filename: "bundle.js"
+      path: path.resolve(__dirname, "../dist"),
+      filename: "bundle.js",
     },
     resolve: {
-
       extensions: [".ts", ".tsx", ".js"]
-    },
-    devServer:{
-      open: true,
-      contentBase: "/dist/bundle.js",
-      port: 3004,
-      historyApiFallback: true,
-      headers: {
-          "Access-Control-Allow-Origin": "*",
-      },
     },
     performance: {
       hints: false,
-      maxAssetSize: 400000,
-      maxEntrypointSize: 4000000,
     },
     module: {
       rules: [
-        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
         { 
             test: /\.tsx?$/, 
             loader: "ts-loader" },
         {
             test: /\.s[ac]ss$/i,
             use: [
-              // Creates `style` nodes from JS strings
               "style-loader",
-              // Translates CSS into CommonJS
               "css-loader",
-              // Compiles Sass to CSS
               "sass-loader",
             ],
           },
@@ -50,5 +37,11 @@ module.exports = {
             ],
           },
       ]
-    }
+    },
+    plugins: [  
+      new HtmlWebpackPlugin({
+          template: __dirname + "/public/index.html",
+          inject: 'body'
+      })
+    ],
   };
