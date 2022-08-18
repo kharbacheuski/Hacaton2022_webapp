e113390b019b8c4b65385db02eb0acea930b3015
Kiryl Harbacheuski <kiryl.harbacheuski@soft-fx.lv>
2022-08-18 16:04:42 +0300

    deleted dev config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,53 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: "./src/index.ts",
-    output: {
-      path: path.resolve(__dirname, "../dist"),
-      filename: "bundle.js",
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "bundle.js",
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  performance: {
+    hints: false,
+  },
+  devServer:{
+    compress: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    resolve: {
-      extensions: [".ts", ".tsx", ".js"]
-    },
-    performance: {
-      hints: false,
-    },
-    module: {
-      rules: [
-        { 
-            test: /\.tsx?$/, 
-            loader: "ts-loader" },
-        {
-            test: /\.s[ac]ss$/i,
-            use: [
-              "style-loader",
-              "css-loader",
-              "sass-loader",
-            ],
-          },
+    port: 3005,
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/, 
+        loader: "ts-loader" },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/i,
+        use: [
           {
-            test: /\.(png|jpg|gif|svg)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-              },
-            ],
+            loader: 'file-loader',
           },
-      ]
-    },
-    plugins: [  
-      new HtmlWebpackPlugin({
-          template: __dirname + "/public/index.html",
-          inject: 'body'
-      })
-    ],
-  };
+        ],
+      },
+    ]
+  },
+  plugins: [  
+    new HtmlWebpackPlugin({
+      template: __dirname + "/public/index.html",
+      inject: 'body'
+    })
+  ],
+};
